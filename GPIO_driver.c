3feaec9c8f1f9a16fc188cbfc266b6a397b10cbf
A K Kavan <kavankulal2254@gmail.com>
2026-09-25 16:57:33 +0530

    blank

--- GPIO_driver.c
+++ GPIO_driver.c
@@ -15,30 +15,38 @@ struct GPIO{
 
 };
 
-struct GPIO *GPIO;
+volatile struct GPIO *GPIO =
+    (volatile struct GPIO *)0x50000000U;
 uint32_t pin;
-void gpio_set_mode(uint32_t pin, uint32_t mode)
+int  gpio_set_mode(uint32_t pin, uint32_t mode)
 {
-    if(pin<GPIO_PIN_COUNT){
+    if(pin<GPIO_PIN_COUNT && mode<=GPIO_ANALOG){
     uint32_t MASK;
     uint32_t VALUE;
     uint32_t SHIFT = pin * 2U;// so pin 0 means 0 then pin 1 means 2 shift and so on
     MASK  = 3U << SHIFT;// this will shift that
-    VALUE = mode << SHIFT;//thiswill shift mode to that position for the value
+    VALUE = mode << SHIFT;//this will shift mode to that position for the value
 
     GPIO->MODE &= ~MASK;// we make it zero for that particular position
     GPIO->MODE |= VALUE;// then we write the mode
+    return 1;
     }
+    return 0;
 }
 
-void gpio_write(uint32_t pin, uint32_t level){
+int  gpio_write(uint32_t pin, uint32_t level){
     if(pin<GPIO_PIN_COUNT ){
-        if (level == GPIO_HIGH)
+        if (level == GPIO_HIGH){
             GPIO->OUTPUT |= (1U << pin);
-        else if(level == GPIO_LOW)
+        return 1;
+        }
+        else if(level == GPIO_LOW){
             GPIO->OUTPUT &= ~(1U << pin);
+            return 1;
+        }
         else{
-            return;
+            return 0;
         }
     }
+    return 0;
 }
