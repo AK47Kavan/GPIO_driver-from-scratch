23d8d4e67420fdfcf584306596be76a3e8420da5
A K Kavan <kavankulal2254@gmail.com>
2026-09-25 16:22:36 +0530

    what if that duffer use my code try to test his idea exactly.. some exception added

--- GPIO_driver.c
+++ GPIO_driver.c
@@ -4,6 +4,9 @@
 #define GPIO_OUTPUT    1U
 #define GPIO_ALTERNATE 2U
 #define GPIO_ANALOG    3U
+#define GPIO_PIN_COUNT 16U
+#define GPIO_HIGH     1U
+#define GPIO_LOW      0U
 
 struct GPIO{
     uint32_t MODE;
@@ -16,6 +19,7 @@ struct GPIO *GPIO;
 uint32_t pin;
 void gpio_set_mode(uint32_t pin, uint32_t mode)
 {
+    if(pin<GPIO_PIN_COUNT){
     uint32_t MASK;
     uint32_t VALUE;
     uint32_t SHIFT = pin * 2U;// so pin 0 means 0 then pin 1 means 2 shift and so on
@@ -24,11 +28,17 @@ void gpio_set_mode(uint32_t pin, uint32_t mode)
 
     GPIO->MODE &= ~MASK;// we make it zero for that particular position
     GPIO->MODE |= VALUE;// then we write the mode
+    }
 }
 
 void gpio_write(uint32_t pin, uint32_t level){
-if (level == GPIO_HIGH)
-    GPIO->OUTPUT |= (1U << pin);
-else
-    GPIO->OUTPUT &= ~(1U << pin);
+    if(pin<GPIO_PIN_COUNT ){
+        if (level == GPIO_HIGH)
+            GPIO->OUTPUT |= (1U << pin);
+        else if(level == GPIO_LOW)
+            GPIO->OUTPUT &= ~(1U << pin);
+        else{
+            return;
+        }
+    }
 }
